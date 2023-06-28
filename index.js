7911d04e5d5e5e6347ee26c98e404f85d6f0a51b
vthrive admin <vthrive@vthrives-MacBook-Pro.local>
2023-06-27 19:44:38 -0500

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("users.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3007; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 3009; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+//NOTE :
+//once you run > npm start
+//goto to browser and check http://localhost:3009/users
